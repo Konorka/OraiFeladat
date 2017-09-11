072661fe29596856efb2c282b926f2742b4302f7
unknown <kovatsnorbert@terem307pc02.athenepraxis.local>
2017-09-11 18:21:04 +0200

    Main View/Base

---
+++ WpfApp9/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp9
+{
+    public class MainViewModel : BaseModel
+    {
+        public ObservableCollection<Product> Product { get; set; }
+        public MainViewModel()
+
+        {
+            Product = new ObservableCollection<Product>
+                {
+                    new Product {},
+                    new Product {}
+            };
+        }
+        public Product SelectedProduct { get; set; }
+    }
+}
+
+
--- WpfApp9/Product.xaml.cs
+++ WpfApp9/Product.xaml.cs
@@ -10,20 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WpfApp9
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Product.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Product : Window
     {
-        public MainWindow()
+        public Product()
         {
             InitializeComponent();
-            DataContext = new MainViewModel();
         }
     }
 }
